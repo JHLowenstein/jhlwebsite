7e428afc029aa7227a00295fa20eca78d1179843
JHLowenstein <jhlGitHub@gmail.com>
2023-07-12 15:14:53 -0400

    update

--- app/(default)/card.tsx
+++ app/(default)/card.tsx
@@ -20,7 +20,6 @@ const CardSection: React.FC<CardSectionProps> = ({
     <div
       className=' group/hero-product flex flex-col items-center p-6 md:p-8 md:m-5 cursor-default bg-white/5 backdrop-blur transition rounded-lg first:rounded-t-3xl last:rounded-b-3xl md:first:rounded-t-lg md:last:rounded-b-lg md:first:!rounded-l-4xl md:last:!rounded-r-4xl hover:scale-[1.02] hover:bg-white/10'
       data-aos='fade-up'
-      data-aos-delay='200'
     >
       <h3 className='flex items-center gap-1 text-zinc-50 font-display text-2xl font-medium leading-none'></h3>
       <Image src={imgSrc} alt={imgAlt} width={120} height={30} unoptimized />
@@ -28,7 +27,7 @@ const CardSection: React.FC<CardSectionProps> = ({
         className='btn text-white bg-blue-600 hover:bg-blue-500 w-full mb-4 sm:w-auto sm:mb-0 rounded-full'
         href={link}
       >
-        <h5 className='h5'>{title}</h5>
+        <h5>{title}</h5>
       </a>
     </div>
   )
--- app/hair/page.tsx
+++ app/hair/page.tsx
@@ -13,7 +13,7 @@ import Particles from '@/app/hair/components/particles'
 
 export default function Hair() {
   return (
-    <>
+    <section>
       <div className='flex flex-col items-center justify-center overflow-hidden '>
         <div className='hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0' />
         <Particles
@@ -22,6 +22,6 @@ export default function Hair() {
         />
         <Hero />
       </div>
-    </>
+    </section>
   )
 }
